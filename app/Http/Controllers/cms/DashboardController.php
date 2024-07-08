03cb3b9a8674aca02ee9e408235b13ab346dfd7a
Avinash-Ukuu <avinash.ukuu@gmail.com>
2024-07-08 09:45:38 +0530

    dashboard in progress

--- app/Http/Controllers/cms/DashboardController.php
+++ app/Http/Controllers/cms/DashboardController.php
@@ -2,13 +2,29 @@
 
 namespace App\Http\Controllers\cms;
 
-use App\Http\Controllers\Controller;
+use App\Models\Course;
+use App\Models\Student;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
 
 class DashboardController extends Controller
 {
     public function dashboard()
     {
-        return view('cms.dashboard');
+        $data['studentCount']           =       Student::count();
+        $data['courseCount']            =       Course::count();
+        $students                       =       Student::whereHas('studentCourse', function ($query) {
+                                                    $query->whereHas('payments', function ($paymentQuery) {
+                                                        $paymentQuery->where('payment_mode', 'installment');
+                                                    });
+                                                })->get()->filter(function ($student) {
+                                                    return $student->hasPendingInstallment();
+                                            });
+
+        $data['totalMonthlyPayment']    =       $students->sum(function ($student) {
+                                                    return $student->studentCourse->monthly_payment;
+                                                });
+
+        return view('cms.dashboard',$data);
     }
 }
--- resources/views/cms/dashboard.blade.php
+++ resources/views/cms/dashboard.blade.php
@@ -1,4 +1,73 @@
 @extends('cms.layouts.master')
 @section('content')
+    <div class="content-header">
+        <div class="container-fluid">
+            <div class="row mb-2">
+                <div class="col">
+                    <ol class="breadcrumb float-sm-right">
+                        <li class="breadcrumb-item active">Dashboard</li>
+                    </ol>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="row ml-2 mr-2">
 
+        <div class="col-lg-4 col-6">
+
+            <div class="small-box bg-warning">
+                <div class="inner">
+                    <h3>{{ $studentCount }}</h3>
+                    <p>Total Students</p>
+                </div>
+                <div class="icon">
+                    <i class="ion ion-person-add"></i>
+                </div>
+                <a href="{{ route('student.index') }}" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+            </div>
+        </div>
+
+        <div class="col-lg-4 col-6">
+
+            <div class="small-box bg-info">
+                <div class="inner">
+                    <h3>{{ $courseCount }}</h3>
+                    <p>Total Courses</p>
+                </div>
+                <div class="icon">
+                    <i class="ion ion-ios-book"></i>
+                </div>
+                <a href="{{ route('course.index') }}" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+            </div>
+        </div>
+
+        <div class="col-lg-4 col-6">
+
+            <div class="small-box bg-success">
+                <div class="inner">
+                    <h3>{{ $totalMonthlyPayment }}<sup style="font-size: 20px">₹</sup></h3>
+                    <p>Monthly Collection</p>
+                </div>
+                <div class="icon">
+                    <i class="ion ion-cash"></i>
+                </div>
+                <a href="{{ route('monthlyCollection') }}" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+            </div>
+        </div>
+
+        {{-- <div class="col-lg-3 col-6">
+
+            <div class="small-box bg-danger">
+                <div class="inner">
+                    <h3>65</h3>
+                    <p>Unique Visitors</p>
+                </div>
+                <div class="icon">
+                    <i class="ion ion-pie-graph"></i>
+                </div>
+                <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+            </div>
+        </div> --}}
+
+    </div>
 @endsection
